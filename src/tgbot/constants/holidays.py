ff472c5c87a91fc6464465d36b1bc4dc67160f45
Maksim Dravitsa <mdravitsa@mail.ru>
2024-04-17 23:29:35 +0300

    Update holidays.py

--- src/tgbot/constants/holidays.py
+++ src/tgbot/constants/holidays.py
@@ -1,8 +1,8 @@
 holidays_2023 = ((2,),  # January
                  (0,),  # February
                  (8,),  # March
-                 (25,),  # April
-                 (1, 9),  # May
+                 (0,),  # April
+                 (1, 9, 14),  # May
                  (0,),  # June
                  (3,),  # July
                  (0,),  # August
