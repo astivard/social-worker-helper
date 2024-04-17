f1e37111453c70e228e8c7e5ffe714474fcc01fd
Maksim Dravitsa <mdravitsa@mail.ru>
2024-04-17 21:51:26 +0300

    Update tariffs.py

--- src/tgbot/constants/tariffs.py
+++ src/tgbot/constants/tariffs.py
@@ -1,7 +1,7 @@
 # (with infrastructure, without infrastructure)
 tariffs = {
-    "unprivileged_person": (1.37, 2.00),
-    "privileged_person": (0.82, 1.20),
-    "married_couples_50": (0.69, 1.00),
-    "married_couples_80": (1.10, 1.60),
+    "unprivileged_person": (1.46, 2.14),
+    "privileged_person": (0.88, 1.28),
+    "married_couples_50": (0.73, 1.07),
+    "married_couples_80": (1.17, 1.71),
 }
